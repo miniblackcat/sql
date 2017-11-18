3d525de229161a466a458ad779418baa09287961
DAVID MASON <dave_mason@rogers.com>
2017-11-17 19:38:08 -0500

    updates

---
+++ errorhandler.py
@@ -0,0 +1,27 @@
+# INSERT Command with Error Handler
+
+# import the sqlite3 library
+import sqlite3
+
+# create the connection object
+conn = sqlite3.connect("new.db")
+
+# get a cursor object used to execute SQL commands
+cursor = conn.cursor()
+
+try:
+	# insert data
+	cursor.execute("INSERT INTO population VALUES('Baltimore', 'MD', 840000)")
+
+	cursor.execute("INSERT INTO population VALUES('Dallas', 'TX' 800000)")
+
+	# commit the changes
+	conn.commit()
+
+except sqlite3.OperationalError:
+	print("Oops! Something went wrong. Try again...")
+
+# close the database connection
+conn.close()
+
+
